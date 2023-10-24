f294f10b7b7ce151adcd0576c88533a0ab8e5e7c
cyj5509 <cyj5509@naver.com>
2023-10-24 19:24:13 +0900

    MemberMepper 주석 처리

--- src/main/java/com/docmall/mapper/MemberMapper.java
+++ src/main/java/com/docmall/mapper/MemberMapper.java
@@ -2,7 +2,15 @@ package com.docmall.mapper;
 
 public interface MemberMapper {
 
-	// 인터페이스는 컴파일 과정에서 자동으로 public이 붙어 생략 가능
-	String idCheck(String mbsp_id);
+/*
+<인터페이스의 사용법>
+
+public interface 인터페이스명{
+    (public static final) 타입 상수명 = 값;
+    (public abstract) 메서드명(매개변수 목록);
+}
+*/
+	
+	String idCheck(String mbsp_id); // public abstract String idCheck(String mbsp_id);
 	
 }
