d9b74a0dd7e1e0fe17be934bb642dfa54ab50435
cyj5509 <cyj5509@naver.com>
2023-10-25 09:20:01 +0900

    매퍼 주석 변경

--- src/main/java/com/docmall/mapper/MemberMapper.java
+++ src/main/java/com/docmall/mapper/MemberMapper.java
@@ -2,7 +2,14 @@ package com.docmall.mapper;
 
 public interface MemberMapper {
 
-	// 인터페이스는 컴파일 과정에서 자동으로 public이 붙어 생략 가능
+	/*
+	<인터페이스의 사용법>
+
+	public interface 인터페이스명{
+	    (public static final) 타입 상수명 = 값;
+	    (public abstract) 메서드명(매개변수 목록);
+	}
+	*/
 	String idCheck(String mbsp_id);
 	
 }
